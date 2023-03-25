43af407d1bec8b8d9d1f4770fbcf0a03f67a0402
Ifeakachukwu Osakwe <ifeakachukwuosakwe@gmail.com>
2023-03-25 13:51:19 +0100

    add

--- lib/bottom_nav_notifier.dart
+++ lib/bottom_nav_notifier.dart
@@ -1,11 +1,26 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final bottomNavNotifierProvider = StateNotifierProvider<BottomNavNotifier, int>(
-  (ref) => BottomNavNotifier(),
+// final bottomNavNotifierProvider = StateNotifierProvider<BottomNavNotifier, int>(
+//   (ref) => BottomNavNotifier(),
+// );
+
+final bottomNavNotifierProvider = NotifierProvider<BottomBarNotifier, int>(
+  () => BottomBarNotifier(),
 );
 
 class BottomNavNotifier extends StateNotifier<int> {
   BottomNavNotifier() : super(0);
+
+  void onTabTapped(int index) {
+    state = index;
+  }
+}
+
+class BottomBarNotifier extends Notifier<int> {
+  @override
+  int build() {
+    return 0;
+  }
 
   void onTabTapped(int index) {
     state = index;
