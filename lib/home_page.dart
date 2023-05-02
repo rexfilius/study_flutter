839f2ecb0d5d5ef4a97062b4550e5002dc093fda
Ifeakachukwu Osakwe <ifeakachukwuosakwe@gmail.com>
2023-05-02 09:52:19 +0100

    add

---
+++ lib/home_page.dart
@@ -0,0 +1,12 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends HookConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
